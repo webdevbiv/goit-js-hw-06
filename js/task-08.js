737f038da6735826948256ba7feb2bb4e8309dd5
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2021-11-09 23:07:27 +0200

    final

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
 
+const refs = {
+    userForm: document.querySelector('.login-form'),
+    userButton: document.querySelector('button')
+}
+// console.dir(refs.userForm);
+// console.log(refs.userButton);
+
+refs.userForm.addEventListener('submit', handleSubmit)
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+    // console.log(email.value);
+    // console.log(password.value);
+
+    if (email.value === '' || password.value === '') {
+        return alert(`Please fill in all the fields!`)
+    }
+    console.log(`Email: ${email.value}\nPassword: ${password.value}`)
+    event.currentTarget.reset();
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,21 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  body: document.querySelector('body'),
+  color: document.querySelector('.color'),
+  button: document.querySelector('.change-color')
+}
+// console.log(refs.body);
+// console.log(refs.color);
+// console.log(refs.button);
+refs.button.addEventListener('click', clickEventHandler)
+function clickEventHandler(event) {
+  // console.log('click');
+  let randomColor = getRandomHexColor()
+  console.log(randomColor);
+  refs.color.innerText = `${randomColor}`
+  refs.body.style.backgroundColor = `${randomColor}`
+
+}
