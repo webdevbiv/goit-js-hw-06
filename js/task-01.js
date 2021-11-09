e94bba8d53e88fbb6ce96ba41ef1182c1c5d4b69
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2021-11-09 10:36:26 +0200

    task2

--- js/task-01.js
+++ js/task-01.js
@@ -3,7 +3,7 @@ function listDataRef() {
     console.log(`Number of categories: ${categoriesRef.length}`);
     // console.dir(categoriesRef)
     const itemsRef = document.querySelectorAll('.item')
-    console.log(itemsRef);
+    // console.log(itemsRef);
     itemsRef.forEach(item => {
         const category = item.querySelector('h2')
         // console.dir(category)
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,9 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listRef = document.getElementById('ingredients')
+console.log(listRef);
+const listItems = ingredients.map((ingredient) => `<li class="item">${ingredient}</li> `).join('')
+console.log(listItems);
+listRef.innerHTML = listItems
