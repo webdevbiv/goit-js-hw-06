098d731ecd04a0b95914b028ced4ce00d6983ed1
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2021-11-09 12:05:37 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -9,6 +9,18 @@ const ingredients = [
 
 const listRef = document.getElementById('ingredients')
 console.log(listRef);
-const listItems = ingredients.map((ingredient) => `<li class="item">${ingredient}</li> `).join('')
-console.log(listItems);
-listRef.innerHTML = listItems
+// const listItems = ingredients.map((ingredient) => `<li class="item">${ingredient}</li> `).join('')
+// console.log(listItems);
+// listRef.innerHTML = listItems
+function newList() {
+  const listArray = []
+  ingredients.forEach(ingredient => {
+    const newItem = document.createElement('li')
+    newItem.classList.add('item')
+    newItem.innerText = ingredient
+    listArray.push(newItem)
+  });
+  console.log(listArray);
+  listRef.append(...listArray)
+}
+newList()
